11711cde9d08591d51c65959ddb06a01365d3a59
ozarhu2049 <94573452+ozarhu2049@users.noreply.github.com>
2022-01-18 15:35:03 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import './style/App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import * as routes from './constants/routes';
+import RedeemVoucher from "./routes/Voucher";
+import LandingPage from "./routes/Landing";
+import SignInPage from "./routes/Signin";
+import TvPage from './routes/SAMSUNG_TV';
+import FootwarePage from "./routes/ASICS_RUNNERS";
+
+
+class App extends Component {
+  render() {
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <Route
+                    exact path={routes.ASICS_RUNNERS}
+                    component={() => <FootwarePage/>}
+                />
+                <Route
+                    exact path={routes.ELECTRONIC}
+                    component={() => <TvPage />}
+                />
+                <Route
+                    exact path={routes.LANDING}
+                    component={() => <LandingPage />}
+                />
+                <Route
+                    exact path={routes.SIGNIN}
+                    component={() => <SignInPage />}
+                />
+                <Route
+                    exact path={routes.VOUCHER}
+                    component={() => <RedeemVoucher />}
+                    />
+            </div>
+        </BrowserRouter>
+    );
+  }
+}
+
+export default App;
